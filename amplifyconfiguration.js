f26d917b916ae04333fe41a08b47deb3e63face2
CasianLW <ciorbcasian@gmail.com>
2024-03-18 09:16:50 +0100

    ampigy config

---
+++ amplifyconfiguration.js
@@ -0,0 +1,6 @@
+export default {
+  aws_project_region: "us-east-1",
+  aws_cognito_region: "us-east-1",
+  aws_user_pools_id: "eu-north-1_FWPMyvA04",
+  aws_user_pools_web_client_id: "i933j4crei746stfplu7iiag1",
+};
