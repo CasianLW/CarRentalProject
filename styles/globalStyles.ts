6df15c0fc0e95a164a3e674af4a790f18df91dc4
CasianLW <ciorbcasian@gmail.com>
2024-03-18 12:11:19 +0100

    global styles & tailwind custom colors

---
+++ styles/globalStyles.ts
@@ -0,0 +1,15 @@
+// globalStyles.ts
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  primaryButton: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f5f5f5",
+  },
+  secondaryButton: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,23 @@ module.exports = {
   ],
 
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "app-black": {
+          200: "#0B0D10",
+          300: "#03050C",
+        },
+        "app-white": {
+          100: "#FFFFFF",
+          200: "#EFEFF0",
+        },
+        "app-copper": {
+          100: "#CCA373",
+          200: "#B08451",
+          300: "#9A6B36",
+        },
+      },
+    },
   },
   plugins: [],
 };
